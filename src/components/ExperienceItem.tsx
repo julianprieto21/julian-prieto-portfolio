5f2b01872b2a4d923121a3699d14b85c31f1e916
Julian Prieto <prietojulian2003@gmail.com>
2024-04-13 20:57:15 -0300

    fix animation and visuals of ExperienceItem

--- src/components/ExperienceItem.tsx
+++ src/components/ExperienceItem.tsx
@@ -5,6 +5,7 @@ interface Props {
     place: string;
     trajectory: { position: string; start: string; finish: string | null }[];
   };
+  index: number;
 }
 
 function ArrowSeparateVertical(props: SVGProps<SVGSVGElement>) {
@@ -49,54 +50,66 @@ function ArrowUnionVertical(props: SVGProps<SVGSVGElement>) {
   );
 }
 
-export default function ExperienceItem({ item }: Props) {
+function TimeLine(props: {
+  trajectory: { position: string; start: string; finish: string | null }[];
+  open: boolean;
+}) {
+  const open = props.open;
+  const trajectoryLength = props.trajectory.length;
+
+  return (
+    <div
+      style={{ height: open ? 40 * trajectoryLength : 0 }}
+      className={`${
+        open ? "opacity-100 my-2" : "opacity-0"
+      } w-full transition-all duration-300 flex flex-row overflow-auto relative ml-3`}
+    >
+      <span className="border-l border-dashed border-primary absolute h-full left-[77px]"></span>
+      <ul className="w-full flex flex-col justify-around">
+        {props.trajectory.map((trajectory, index) => (
+          <li
+            key={index}
+            className="flex flex-row justify-start gap-2 items-center text-secondary"
+          >
+            <span>{trajectory.start}</span>
+            <span className="w-3 h-3 rounded-full bg-primary" />
+            <span className="font-bold text-lg">{trajectory.position}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default function ExperienceItem({ item, index }: Props) {
   const [open, setOpen] = useState(false);
   const trajectoryLength = item.trajectory.length;
   const first = item.trajectory[0];
   const last = item.trajectory[trajectoryLength - 1];
 
   return (
-    <li key={item.place}>
-      <ul
-        className={`${
-          open ? "opacity-100" : "opacity-0"
-        } w-full transition-all flex flex-col justify-end delay-100`}
-      >
-        {item.trajectory
-          .slice(0, trajectoryLength - 1)
-          .map((trajectory, index) => (
-            <>
-              <li
-                key={index}
-                className={`flex flex-row items-center justify-start transition-all delay-100 border-r border-secondary/10 ${
-                  open ? "h-[60px]" : "h-0"
-                }`}
-              >
-                <span className="w-4 h-4 rounded-full bg-primary mr-4" />
-                <main>
-                  <h2 className="text-2xl font-medium">
-                    {trajectory.start} - {trajectory.finish}
-                  </h2>
-                  <h3 className="font-bold text-secondary text-lg">
-                    {trajectory.position} | {item.place}
-                  </h3>
-                </main>
-              </li>
-              {item.trajectory[index + 1] ? (
-                <li className="border-l border-dashed border-primary h-8 ml-[7px]" />
-              ) : null}
-            </>
-          ))}
-      </ul>
+    <li key={index} className="border-b border-secondary pb-0.5">
       <div className="flex flex-row items-center justify-start relative">
-        <span className="w-4 h-4 rounded-full bg-primary mr-4" />
         <main>
-          <h2 className={`text-2xl font-medium `}>
-            {open ? last.start : first.start} -{" "}
-            {last.finish ? last.finish : "Actualidad"}
-          </h2>
-          <h3 className={`font-bold text-secondary text-lg `}>
-            {last.position} | {item.place}
+          <a
+            href="https://seidoranalytics.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2
+              className={`text-2xl font-semibold transition duration-300 hover:underline ${
+                open ? "translate-y-5" : "translate-y-0"
+              }`}
+            >
+              .{item.place}
+            </h2>
+          </a>
+          <h3
+            className={`font-bold text-secondary text-lg transition duration-150 pl-3 ${
+              open ? "opacity-0" : "opacity-100"
+            }`}
+          >
+            {first.start} - {last.finish ? last.finish : "Actualidad"}
           </h3>
         </main>
         <button
@@ -112,6 +125,39 @@ export default function ExperienceItem({ item }: Props) {
           )}
         </button>
       </div>
+      <TimeLine trajectory={item.trajectory} open={open} />
+
+      {/* <ul
+        className={`${
+          open ? "opacity-100" : "opacity-0"
+        } w-full transition-all flex flex-col justify-end delay-100 overflow-auto`}
+      >
+        {item.trajectory
+          .slice(0, trajectoryLength - 1)
+          .map((trajectory, index) => (
+            <li
+              key={index}
+              className={`flex flex-col justify-center items-start transition-all delay-100  ${
+                open ? "h-[92px]" : "h-0"
+              }`}
+            >
+              <main className="flex flex-row items-center border-r border-secondary/10 w-full">
+                <span className="w-4 h-4 rounded-full bg-primary mr-4" />
+                <section>
+                  <h2 className="text-2xl font-medium">
+                    {trajectory.start} - {trajectory.finish}
+                  </h2>
+                  <h3 className="font-bold text-secondary text-lg">
+                    {trajectory.position} | {item.place}
+                  </h3>
+                </section>
+              </main>
+              {item.trajectory[index + 1] ? (
+                <span className="border-l border-dashed border-primary h-8 ml-[7px]" />
+              ) : null}
+            </li>
+          ))}
+      </ul> */}
     </li>
   );
 }
